43e63af21d6e5f7735a0cdd98de61405f7a9262d
NatandeAntoni <105368636+NatandeAntoni@users.noreply.github.com>
2024-03-21 08:46:45 -0300

    save

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-const contadores = document.querySelectorAll(".contador");
 
 for(let i=0; i <botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
-
+            
         }
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-06-01T00:00:00");
-contadores[0] .textContent = tempoObjetivo1;
+let tempoAtual = new Date();
+contadores[0] .textContent = tempoObjetivo1 - tempoAtual;
