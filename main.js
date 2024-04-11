4eaef866a8a798a3f7b8fb482ed03e9325af3ee5
NatandeAntoni <105368636+NatandeAntoni@users.noreply.github.com>
2024-04-11 08:45:19 -0300

    save

--- main.js
+++ main.js
@@ -15,12 +15,13 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo4 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-04T00:00:00");
+const tempoObjetivo2 = new Date("2025-03-02T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-11-01T00:00:00");
 
-const tempos = {tempoObjetivo1, tempoObjetivo2, tempoObjetivo3,tempoObjetivo4};
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
@@ -35,6 +36,9 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
- 
- return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-}
+
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    }else{
+        return "Prazo encerrado";
+    }
