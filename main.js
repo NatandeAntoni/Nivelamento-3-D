57db746980b16eb77e5985e06c5b25322dea9754
NatandeAntoni <105368636+NatandeAntoni@users.noreply.github.com>
2024-04-11 08:30:57 -0300

    save

--- main.js
+++ main.js
@@ -16,8 +16,11 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo4 = new Date("2023-10-05T00:00:00");
 
-
+const tempos = {tempoObjetivo1, tempoObjetivo2, tempoObjetivo3,tempoObjetivo4};
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
