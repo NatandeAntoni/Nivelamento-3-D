3634ef8f5a45b61bc2790e64a8dd7020f4a44234
NatandeAntoni <105368636+NatandeAntoni@users.noreply.github.com>
2024-04-11 08:23:33 -0300

    save

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0; i <botoes.length; i++){
-    botoes[i].onclick = function(){
-        for(let j=0; j<botoes.length; j++){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
-            
         }
+
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
+
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-06-01T00:00:00");
-let tempoAtual = new Date();
-contadores[0] .textContent = tempoObjetivo1 - tempoAtual;
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
