af64026d5fba68f94a0c2ecfbab6418279a81ff7
NatandeAntoni <105368636+NatandeAntoni@users.noreply.github.com>
2024-04-11 19:31:41 -0300

    save

--- main.js
+++ main.js
@@ -22,7 +22,6 @@ const tempoObjetivo4 = new Date("2024-11-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
