160992f3a04a4da8505688563a6cce42a954ead6
NatandeAntoni <105368636+NatandeAntoni@users.noreply.github.com>
2024-04-11 08:19:48 -0300

    save

--- main.js
+++ main.js
@@ -16,3 +16,17 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-06-01T00:00:00");
 let tempoAtual = new Date();
 contadores[0] .textContent = tempoObjetivo1 - tempoAtual;
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+ 
+ return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+}
